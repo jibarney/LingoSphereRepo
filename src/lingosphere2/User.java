54f746f1b879376dc3e4f22ccdf737d57d41aba9
jibarney <joann_barney@yahoo.com>
2016-02-01 22:39:45 -0500

    Added login panel functionality

--- src/lingosphere2/User.java
+++ src/lingosphere2/User.java
@@ -5,10 +5,35 @@
  */
 package lingosphere2;
 
-/**
- *
- * @author JoAnn
- */
+import java.util.ArrayList;
+enum UserType { STUDENT, INSTRUCTOR};
+   
 public class User {
-    
+     private String username;
+     private String psswd ;
+     private String classid ;
+     private String usertype;
+     private int grade ;
+     
+     public User() { };
+     public User(String username,String psswd,String classid, String usertype)
+     {
+         this.username = username;
+         this.psswd = psswd;
+         this.classid = classid;
+         this.usertype = usertype;
+     }
+     public Boolean checkValidUser(String usernameInput, String psswdInput)
+     { 
+         if ((usernameInput.trim().equals(username))&&
+                 psswdInput.trim().equals(psswd))
+                 return true ;
+         return false ;
+     };
+     public void setUsername(String username){ this.username = username ; }
+     public void setPsswd(String psswd){ this.psswd = psswd ; }
+     public void setClass(String classid) { this.classid = classid ; }
+     public void setType(String userType) { this.usertype = usertype; }
+  
+     
 }
